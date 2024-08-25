2f07933c32aaee304efeb12ceeea33c0bf7a7f5e
projectskl17 <hermit0bot@gmail.com>
2024-08-25 22:57:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ module.exports = {
     BOT_TOKEN: process.env.BOT_TOKEN || '',
     MONGO_URI_TOKENS: process.env.MONGO_URI_TOKENS || '',
     MONGO_URI_PRODUCTS: process.env.MONGO_URI_PRODUCTS || '',
-    DOMAIN: process.env.DOMAIN || 'https://probot.loca.lt',
+    DOMAIN: process.env.DOMAIN || '',
     PORT: process.env.PORT || 3000,
     ADMINS: process.env.ADMINS ? process.env.ADMINS.split(',') : [],
 };
